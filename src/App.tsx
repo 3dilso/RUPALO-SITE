ee6f064ee1b369549ae2808b4e5b7645e3a3514e
[dyad] <edilsonpaul9@gmail.com>
2026-01-30 10:37:07 +0100

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -14,22 +14,21 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <> {/* Adicionado um React.Fragment para envolver os múltiplos filhos */}
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/sobre" element={<Sobre />} />
-            <Route path="/servicos" element={<Servicos />} />
-            <Route path="/galeria" element={<Galeria />} />
-            <Route path="/contacto" element={<Contacto />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </>
+    {/* Toaster e Sonner são componentes globais e não precisam estar dentro do TooltipProvider */}
+    <Toaster />
+    <Sonner />
+    <TooltipProvider> {/* O TooltipProvider agora envolve apenas o BrowserRouter */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/servicos" element={<Servicos />} />
+          <Route path="/galeria" element={<Galeria />} />
+          <Route path="/contacto" element={<Contacto />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
